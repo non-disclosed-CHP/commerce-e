d743e5ec2ca3469c1d07bd0717413abdf75efb39
non-disclosed-CHP <popsicletomato@gmail.com>
2024-12-12 01:41:41 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example script to handle Add to Cart functionality
+document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Product added to cart!');
+  });
+});
